75fafc0df91684063c18736b3c888f385fd3d6ee
Sirinya <sry.ongkh@gmail.com>
2024-03-10 18:57:04 +0700

    Add icon web

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,34 @@
+gsap.registerPlugin(ScrollTrigger);
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+  tablet: { smooth: true },
+  smartphone: { smooth: true },
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  },
+  pinType: document.querySelector("#main").style.transform
+    ? "transform"
+    : "fixed",
+});
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.refresh();
 function cursorEffect() {
   let heroContent = document.querySelector("#hero-content");
   let cursor = document.querySelector("#cursor");
-
   heroContent.addEventListener("mousemove", function (dets) {
     gsap.to(cursor, {
       x: dets.x,
@@ -21,5 +48,20 @@ function cursorEffect() {
     });
   });
 }
+function pageContentAnimation() {
+  gsap.from(".elem h2", {
+    y: 120,
+    stagger: 0.25,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#content-introduce",
+      scroller: "#main",
+      start: "top 40%",
+      stop: "top 37%",
+      scrub: 1,
+    },
+  });
+}
 
-cursorEffect()
+cursorEffect();
+pageContentAnimation();
