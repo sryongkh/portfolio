9e56bc5f969ed1209e6be4da9570017934192277
Sirinya <sry.ongkh@gmail.com>
2024-03-11 18:28:01 +0700

    Add project section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,138 @@
-gsap.registerPlugin(ScrollTrigger);
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-  tablet: { smooth: true },
-  smartphone: { smooth: true },
-});
-locoScroll.on("scroll", ScrollTrigger.update);
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length
-      ? locoScroll.scrollTo(value, 0, 0)
-      : locoScroll.scroll.instance.scroll.y;
-  },
-  getBoundingClientRect() {
-    return {
-      top: 0,
-      left: 0,
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  },
-  pinType: document.querySelector("#main").style.transform
-    ? "transform"
-    : "fixed",
-});
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-ScrollTrigger.refresh();
+// Locomotive
+function locoScroll() {
+  gsap.registerPlugin(ScrollTrigger);
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+    tablet: { smooth: true },
+    smartphone: { smooth: true },
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.refresh();
+}
+locoScroll();
 function cursorEffect() {
   let heroContent = document.querySelector("#hero-content");
   let cursor = document.querySelector("#cursor");
+
   heroContent.addEventListener("mousemove", function (dets) {
     gsap.to(cursor, {
-      x: dets.x,
-      y: dets.y,
+      css: { x: dets.clientX, y: dets.clientY },
+      duration: 0.3,
     });
   });
-  heroContent.addEventListener("mouseenter", function (dets) {
+  heroContent.addEventListener("mouseenter", function () {
     gsap.to(cursor, {
       scale: 1,
       opacity: 1,
+      duration: 0.3,
     });
   });
-  heroContent.addEventListener("mouseleave", function (dets) {
+  heroContent.addEventListener("mouseleave", function () {
     gsap.to(cursor, {
       scale: 0,
       opacity: 0,
+      duration: 0.3,
     });
   });
 }
+cursorEffect();
 function pageContentAnimation() {
-  gsap.from(".elem h2", {
-    y: 120,
-    stagger: 0.25,
-    duration: 1,
+  gsap.from(".elem h2, #content-introduce-top h3 span", {
+    y: 100,
+    opacity: 0,
+    stagger: 0.1,
+    duration: 0.4,
     scrollTrigger: {
       trigger: "#content-introduce",
       scroller: "#main",
-      start: "top 40%",
-      stop: "top 37%",
-      scrub: 1,
+      start: "center bottom",
+      end: "center center",
+      scrub: true,
     },
   });
+  gsap.from("hr", {
+    scrollTrigger: {
+      trigger: "hr",
+      scroller: "#main",
+      opacity: 0,
+      scrub: true,
+      start: "center bottom",
+      end: "center center",
+    },
+    scaleX: 0,
+    transformOrigin: "left center",
+    ease: "none",
+  });
+  gsap.from("#work-page-top", {
+    y: 100,
+    opacity: 0,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: "#work-page",
+      scroller: "#main",
+      start: "top center",
+      end: "top top",
+      // scrub: true,
+    },
+  });
+  gsap.to("#work-elements .box", {
+    y: -80,
+    stagger: 0.15,
+    opacity: 1,
+    duration: 0.4,
+    scrollTrigger: {
+      trigger: "#work-page",
+      scroller: "#main",
+      start: "top center",
+      end: "top top",
+      scrub: true,
+    },
+  })
 }
-
-cursorEffect();
 pageContentAnimation();
+// Loader
+let tl = gsap.timeline();
+tl.from("#loader h3", {
+  x: 20,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.1,
+});
+tl.to("#loader h3", {
+  opacity: 0,
+  x: -20,
+  stagger: 0.1,
+});
+tl.to("#loader", {
+  opacity: 0,
+});
+tl.to("#loader", {
+  display: "none"
+})
+// Hero
+tl.from("#hero-content h1 span", {
+  y: 100,
+  opacity: 0,
+  stagger: 0.2,
+  duration: 0.5,
+});
