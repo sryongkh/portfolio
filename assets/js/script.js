077c757a332bdd5344cd01fb0f92298a9ece2429
Sirinya <sry.ongkh@gmail.com>
2024-03-23 05:11:05 +0700

    Update projects, contacts, build responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,34 +132,125 @@ function pageContentAnimation() {
       scrub: true,
     },
   });
-  gsap.to("#work-elements .box", {
-    y: -80,
-    stagger: 0.15,
-    opacity: 1,
-    duration: 0.4,
-    scrollTrigger: {
-      trigger: "#work-page",
-      scroller: "#main",
-      start: "top center",
-      end: "top top",
-      scrub: true,
-    },
-  });
-  gsap.from(".contact-list .contact-item", {
-    y: -80,
-    stagger: 0.15,
-    opacity: 0,
-    duration: 0.4,
-    scrollTrigger: {
-      trigger: "#contact-page",
-      scroller: "#main",
-      start: "top center",
-      end: "top top",
-      scrub: true,
-    },
+  const contactBtn = document.getElementById("contactLink");
+  contactBtn.addEventListener("click", function () {
+    gsap.to(window, {
+      duration: 1,
+      scrollTo: {
+        y: "#contact-page",
+        offsetY: 0,
+      },
+    });
   });
 }
 pageContentAnimation();
+function addBoxToWorkElements() {
+  document.addEventListener("DOMContentLoaded", function () {
+    fetch("/assets/data/project.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const workElements = document.getElementById("work-elements"); // ย้ายมานอก loop
+        data.forEach((item) => {
+          // Box
+          const boxLink = document.createElement("a");
+          boxLink.setAttribute("class", "box");
+          boxLink.setAttribute("href", item.link);
+          // Badge elements
+          const techStack = document.createElement("div");
+          techStack.setAttribute("class", "techstack");
+          const techItems = item.stack;
+          techItems.forEach((tech) => {
+            // เปลี่ยนชื่อเพื่อไม่ให้สับสนกับ 'item'
+            const techItem = document.createElement("div");
+            techItem.setAttribute("class", "tech-item");
+            techItem.textContent = tech;
+            techStack.appendChild(techItem);
+          });
+          // Logo
+          const logoImg = document.createElement("img");
+          logoImg.setAttribute("src", item.logo);
+          logoImg.setAttribute("class", "logo");
+          // Image
+          const imgWrapper = document.createElement("div");
+          imgWrapper.setAttribute("class", "img-wrapper");
+          const coverImg = document.createElement("img");
+          coverImg.setAttribute("src", item.img);
+          imgWrapper.appendChild(coverImg);
+          // Video
+          const video = document.createElement("video");
+          video.setAttribute("src", item.video);
+          video.setAttribute("autoplay", "");
+          video.setAttribute("loop", "");
+          video.setAttribute("muted", "");
+          // Append
+          boxLink.appendChild(techStack);
+          boxLink.appendChild(logoImg);
+          boxLink.appendChild(imgWrapper);
+          boxLink.appendChild(video);
+
+          // Append ไปยัง workElements ใน loop
+          workElements.appendChild(boxLink);
+        });
+        // GSAP
+        gsap.to("#work-elements .box", {
+          y: -80,
+          stagger: 0.15,
+          opacity: 1,
+          duration: 0.2,
+          scrollTrigger: {
+            trigger: "#work-page",
+            scroller: "#main",
+            start: "top center",
+            end: "top top",
+          },
+        });
+      })
+      .catch((error) => console.error("Error fetching JSON data:", error));
+  });
+}
+addBoxToWorkElements();
+function addContactItem() {
+  document.addEventListener("DOMContentLoaded", () => {
+    fetch("/assets/data/contact.json")
+      .then((response) => response.json())
+      .then((data) => {
+        const contactContainer = document.getElementById("contact-page");
+        data.forEach((item) => {
+          const link = document.createElement("a");
+          link.setAttribute("href", item.link);
+          link.setAttribute("class", "contact-list");
+          const container = document.createElement("div");
+          container.setAttribute("class", "contact-item");
+          const h3 = document.createElement("h3");
+          h3.textContent = item.name;
+          const line = document.createElement("div");
+          line.setAttribute("class", "line-hv");
+          container.appendChild(h3);
+          container.appendChild(line);
+          link.appendChild(container);
+          contactContainer.appendChild(link);
+        });
+        // GSAP
+        gsap.from(".contact-list .contact-item", {
+          y: -80,
+          stagger: 0.15,
+          opacity: 0,
+          duration: 0.4,
+          scrollTrigger: {
+            trigger: "#contact-page",
+            scroller: "#main",
+            start: "top center",
+            end: "top top",
+            scrub: true,
+          },
+        });
+
+        ScrollTrigger.refresh();
+      });
+  });
+}
+addContactItem();
+
 // Loader
 let tl = gsap.timeline();
 tl.from("#loader h3", {
